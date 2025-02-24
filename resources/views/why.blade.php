a7025f8dcb5456d1854e389db740903e11e8ba93
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-02-24 15:18:45 +0200

    todo skati un routes

--- resources/views/why.blade.php
+++ resources/views/why.blade.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>WHYYEYYYEYE</title>
 </head>
 <body>
-    
-<h1>Sveisksks LARAVEL</h1>
-
+    <h1>whwhwhwhwhy</h1>
 </body>
 </html>
